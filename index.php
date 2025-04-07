f4efaace211b1f6ae6cfa2ea86d1bfbf592ba59f
andreluiskunert <pesquisador036@gmail.com>
2025-04-07 16:30:51 -0300

    arquivosAdicionados

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+   <header>
+    <div class="cabecalho">
+       
+    </div>
+</header>
+<body>
+    <div class="corpo">
+
+    </div>
+    
+</body>
+ <footer>
+    <div class="rodape">
+
+    </div>
+ </footer>
+</html>
