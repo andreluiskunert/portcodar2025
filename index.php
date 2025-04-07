4c878b2b7d31c802f35879ca3a6d43e332c81d0c
andreluiskunert <pesquisador036@gmail.com>
2025-04-07 16:39:34 -0300

    arquivosAdicionados02

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/css/estilo.css">
+    <title>Inicio</title>
+
 </head>
    <header>
     <div class="cabecalho">
